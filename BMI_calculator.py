5ee532b8ef792cbcac59edbcdced0d118ab94c4e
ROME <romanojhaojha@gmail.com>
2021-02-09 22:46:40 +0545

    finalize BMI calculator

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,14 +1,40 @@
+import itertools
+import threading
+import time
+import sys
+
+done = False
+# animation
+
+
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rloading your' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rsurvey completed')
+
+
 weight = input("enter your weight in Kilo Gram(KG)\n")
 height = input("enter your height in meters\n")
 float_height = float(height)
-BMI = float(weight)/(float_height ** float_height)
+BMI = float(weight)/(float_height ** 2)
 BMI = int(BMI)
-print("your BMI is", BMI)
+# animation part strt
+# long process here
+t = threading.Thread(target=animate)
+t.start()
+time.sleep(2)
+done = True
+# animtaion part ends here
+print("\nyour BMI is", BMI)
 if 0 <= BMI <= 18.5:
-    print("you are underweight")
+    print("you are underweight,\n please improve your protein intake")
 elif 18.6 <= BMI <= 25:
-    print("you are normal weight")
+    print("you are normal weight,\n stay as you are and exercise")
 elif 25.1 <= BMI <= 30:
-    print("you are overweight")
+    print("you are overweight,\n you must know what its like to live in somalia")
 else:
     print("you are too much, you will die of obesity")
