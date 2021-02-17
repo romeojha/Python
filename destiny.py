a0e8e7784280d431baebf17228f50a7cd0c1af3d
ROME <romanojhaojha@gmail.com>
2021-02-17 22:50:03 +0545

    modified destiny

--- destiny.py
+++ destiny.py
@@ -2,14 +2,14 @@ play = input(
     "this is the adventure destiny game.\nEnter 'play' or 'exit' to do the same\n")
 print(''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        __ _ __`~^~`~^^~`~^~`~^^~`~^~`~^^~____    ,~~`~~_`~^~`~^^~`~^~`~^^~`~^~`~^^~
-   ,'`  /     \~-~^~^~`~^~` ____________/    \,-`        /`~^~`~^^~`~^~`~^^~`~^~`~^^~
- ,'     |       ~~boat\/~~~                             /`~^~`~^^~`~^~`~^^~`~^~`~^^~
-(destiny|----| |\^~^~`~^~`/                            /v`~^~`~^^~`~^~`~^^~`~^~`~^^~
- \   ,,,|_       /~~^~^^~/  \/Broken      # #    \/Newcastle`~^~`~^^~`~^~`~^^~
-  (_/    `^|`~, / ~^~^~`|     Hill     # # # # #     /`~^~`~^^~`~^~`~^^~`~^~`~^^~
-`~^~`~^^~`~^~`~^^~ `~^~`|              #    #    \/start`~^~`~^^~`~^~`~^^~`~^~`~^^~
-`~~`~^~`~^^~~^~`~^~`~^^.|              #   # # #   / `~^~`~^^~`~^~`~^^~`~^~`~^^~
-`~^~`~^^~`~^~`~^^~`~^~,' \_____        #  #  #    /`~^~`~^^~`~^~`~^^~`~^~`~^^~
+   ,'`  /      \~-~^~^~`~^~` ____________/    \,-`        /`~^~`~^^~`~^~`~^^~`~^~`~^^~
+ ,'     |       \~~boat\/~~~ \/Beach                     /`~^~`~^^~`~^~`~^^~`~^~`~^^~
+(destiny|        \^~^~`~^~`/                            /v`~^~`~^^~`~^~`~^^~`~^~`~^^~
+ \   _ _|        /~~^~^^~/                       \/Newcastle`~^~`~^^~`~^~`~^^~
+  (_/   |__ _ __/ ~^~^~`| \/brokenHill  # # # #       /`~^~`~^^~`~^~`~^^~`~^~`~^^~
+`~^~`~^^~`~^~`~^^~ `~^~`|              #    #       /`~^~`~^^~`~^~`~^^~`~^~`~^^~
+`~~`~^~`~^^~~^~`~^~`~^^.|              #   #       / `~^~`~^^~`~^~`~^^~`~^~`~^^~
+`~^~`~^^~`~^~`~^^~`~^~,' \_____        #  #    \/start`~^~`~^^~`~^~`~^^~`~^~`~^^~
  `~^~`~^^`~^~`~^^~`~^~`~^^~`~^~'\_,~~~, # #      /`~^~`~^^~`~^~`~^^~`~^~`~^^~
   ~~-~^~^~`~^~`~^^~^~-^~^~^~-~`~^~``~  \_#      /`~^~`~^^~`~^~`~^^~`~^~`~^^~
  ~^~ ~~-^`~^~`~^^~~^~-^~^~^~-~^~^`~^~`~^^\_|   /`~^~`~^^~`~^~`~^^~`~^~`~^^~
@@ -17,9 +17,9 @@ print(''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 if play == 'play':
-    print("Rules/process\n 1.you must avoid hash and look map to find the way out\n 2.you only have one half of map to guide you\n 3.you will get real world rewards if you reach your destiny\n 4.you must survive")
-    enter = input('are you sure want to enter?(y/n)\n')
-    if enter == 'y':
+    print("Rules/process\n 1.you must avoid hash and look map to find the way out\n 2.you only have one half of map to guide you\n 3.you will get real world rewards if you reach your destiny\n 4.each travel takes one day")
+    male_female = input("enter :'male'\n")
+    if male_female == 'male':
         print('''
                             ___/""\     
                                oo##
@@ -32,9 +32,9 @@ if play == 'play':
                             ..--:: :..
                             |..|   |..| ''')
         r_or_left = input(
-            "you are at the start point(look given map for reference).\nyou have handful of foods to survive on this journey\n\n Take 'left' or 'right'?")
+            "you are at the start point facing the hash(###)(look given map for reference).\nyou have handful of foods to survive on this journey\n\n Take 'left' or 'right'?")
         if r_or_left == 'right':
-            print("you reached New castle after a day.")
+            print("you reached New castle after a day of travel.")
             print('''
                                                                               
                                                                               
@@ -84,7 +84,7 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
                 print("your have bag full of food and you have rope with you.")
                 r_or_left = input("do you want to go 'left' or 'right'?")
                 if r_or_left == 'left':
-                    print("you travelled for two days to reach the beach")
+                    print("you travelled for one day to reach the beach")
                     print('''
         ... ....                     .. ...           
   .. ...... .                           ........__    
@@ -99,11 +99,8 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
         _ -__--__--_ -              |:     ::.        
           - _--__- _                |/       ::.      
            _ --__ -                 /'         :`--.__
-            _ -_ -                 /'   ._ .     ``   
-             --__              _,-'   __/. .... .  .  
-              -_         __,--/'   __/##`-._____,----'
-      .--.
-    _/____\_              
+      .--.  _ -_ -                 /'   ._ .     ``   
+    _/____\_               _,-'   __/. .... .  .      _              
       |OO|
       |()|
  .----\""/----.
@@ -119,7 +116,7 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
    
 ''')
 
-                    print("you reached the beach. \n there is a boat in middle of river , a trader on the beach and  a sign directing to broken hill.\n 1.do you want to 'swim' to get the boat?\n 2.do you want to 'trade' your leftover foods with trader for new boat?\n 3.do you want to go to 'brokenhill' to collect foods?")
+                    print("you reached the beach,there is a boat in middle of river(but the sea is vast)\na trader on the beach\n a sign directing to broken hill(unknown land with no proof of survival).\n 1.do you want to 'swim' to get the boat?\n 2.do you want to 'trade' your leftover foods with trader for new boat?\n 3.do you want to go to 'brokenhill' to collect foods?")
                     s_t_b = input("enter 'swim' or 'trade' or 'brokenhill'\n")
                     if s_t_b == 'trade':
                         print(
@@ -137,7 +134,7 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
    ~~-~^~~^~^^~`~^~`~^^~^~-^~^~^~-^~`~^~`~^^~^~-^~^~^~-^~`~^~`~^^~^~-^~^~^~-
 ''')
                         enter = input(
-                            "you reached this new bizzare island after three days.\n no food, no animals but something strange about this place\n do you want to continue?(y/n)\n")
+                            "you reached this new bizzare island after one day.\n no food, no animals but something strange about this place\n do you want to continue?(y/n)\n")
                         print('''
 
 
@@ -155,9 +152,9 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
          \:\   '.':. \#//\\
           ':|    "--'(#///)
                      (#///)
-                     (#///)         ___/""\     
-                      \#///\           oo##
-                      (##///)         `-6 #
+                     (#///)            /""\     
+                      \#///\           #oo#
+                      (##///)          \__/
                       (##///)          ,'`.
                       (##///)         // `\\
                       (##///)        ||o   \\
@@ -175,7 +172,7 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
 
                         if enter == 'y':
                             print(
-                                "you are hungry.after some time,you found three way and three boards. straight says'4days' left says'3days' right says'5days'")
+                                "you are hungry.after walking for one day,you found three path  and three boards\nstraight says'4days' left says'3days' right says'5days'.they all meant your survival days from begining")
                             threeway = input(
                                 "do you want to go 'straight' or ' right' or 'left' ?")
                             if threeway == 'straight':
@@ -230,10 +227,10 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
                             print("you quit at final stage")
                     elif s_t_b == 'swim':
                         print(
-                            "you went to swimming to get the nearest boat and died by poision")
+                            "you went to swimming to get the nearest boat and killed by sea animals")
                     elif s_t_b == 'brokenhill':
                         print(
-                            'it took 4 days to reach broken hill.\nnothing in broken hill. your only option is to return')
+                            'it took one day to reach broken hill.\nnothing in broken hill. your only option is to return')
                         print('you die with starvation')
                     else:
                         print("you pressed wrong")
@@ -246,10 +243,10 @@ __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |
                     print("you are out of map. you will die starving")
                 else:
                     print(
-                        "you choose right path but you are starving after 45hours and cannot survive this game.\nyou will die soon")
+                        "you choose right path but you are starving and cannot survive this game.\nyou will die soon")
         else:
             print("you died. Hash kiled you ")
     else:
-        print("you quit, you cannot try again")
+        print("you quit, you cannot try again or try again if you like")
 else:
-    print("you quit, you cannot try again")
+    print("you quit, you cannot try again or try again if you like")
