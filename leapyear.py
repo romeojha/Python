0a17c1fb29293f3711af9fc891f193cca65557f8
romeojha <romanojhaojha@gmail.com>
2021-05-28 02:47:22 -0400

    improved leapyear

--- leapyear.py
+++ leapyear.py
@@ -11,25 +11,26 @@
 
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+
 def is_leapyear(year):
     """check leapyear and returns true or false"""
-    if (year%4==0 and year%100!=0) or year%400==0:
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         return True
     else:
         return False
 
-def days_in_month(year,month):
+
+def days_in_month(year, month):
     """if leapyear, returns month 2nd as 29"""
-    if not 1<=month<=12:
+    if not 1 <= month <= 12:
         return 'not a month'
-    if month==2 and is_leapyear(year):
+    if month == 2 and is_leapyear(year):
         return 29
     else:
-        return month_days[month-1]       
-    
+        return month_days[month - 1]
+
+
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
-
-
