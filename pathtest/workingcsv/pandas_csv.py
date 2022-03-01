d83a678511c4c3a22156df7536c6c45431c860de
romeojha <romanojhaojha@gmail.com>
2022-02-28 23:49:00 -0500

    added some changes

--- pathtest/workingcsv/pandas_csv.py
+++ pathtest/workingcsv/pandas_csv.py
@@ -1,9 +1,8 @@
-
-import pandas
-
-data=pandas.read_csv("./weathercsv.csv")
-print(data[data.day == 'Sunday'])
-print(data.day)
+#find the number of grey squarrel from the given file of squarre count.csv
+import pandas as pd
+data=pd.read_csv('squarrel_count.csv')
+listed_val=len(data[data["Primary Fur Color"] == 'Gray'])
+print(listed_val)
 
 
 
@@ -15,13 +14,11 @@ print(data.day)
 # print(sum(to_list)/len(to_list))
 # anotherdata=0
 
+# import pandas
 
-
-
-
-
-
-
+# data=pandas.read_csv("./weathercsv.csv")
+# print(data[data.day == 'Sunday'])
+# print(data.day)
 
 # for data in to_list:
 #     if data>anotherdata:
@@ -36,4 +33,3 @@ print(data.day)
 #     for r in csv_data:
 #         print(r)
 #         temp.append(r[1])
-
