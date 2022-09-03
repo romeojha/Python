55790afd7440f1bf4e416dce0dbf67664050cb49
ROME <75425506+romeojha@users.noreply.github.com>
2022-09-02 23:55:39 -0400

    random digit frequency
    check frequency of random digits

--- import random.py
+++ import random.py
@@ -6,9 +6,9 @@ with open('new.csv','w') as f:
     write=csv.writer(f)
     for j in range(1000):
         r=random.randrange(0,50)
-        ind=random.randrange(0,20)
-        for i in range(2):
-            a=[r,ind]
-        write.writerow(a)
+#        ind=random.randrange(0,20)
+#        for i in range(2):
+#            a=[r,ind]
+        write.writerow(r)
     
     
